745f37925eb9ceb1d4fe9ddba03648a65dd79eaf
Scott Thiessen <ScottyThiessen@gmail.com>
2015-04-21 09:27:52 -0700

    fix merge conflict

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-add server js...
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(request, response) {
+  response.status(404).sendfile(__dirname + '/public/404.html');
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running at localhost: ' + app.get('port'));
+});
