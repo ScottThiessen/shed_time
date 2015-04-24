b62ae2c0ef52d2970eca48d10f86a7a5f4fa01c1
Scott Thiessen <ScottyThiessen@gmail.com>
2015-04-23 21:47:29 -0700

    fix broken whatever

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,5 @@
 'use strict';
 
-$(function(){
-
-  //------session storage---------\\
-  function save(){
-    var checkbox = document.getElementById('drums2-2');
-    localStorage.setItem('drums2-2', checkbox.checked);
-  }
-
-  function load(){
-    var checked = JSON.parse(localStorage.getItem('drums2-2'));
-    // document.getElementById("drums2-2").checked = checked;
-  }
-
-  load();
-
-  $('#drums2').click(function() {
-    save();
-  })
-  //^^^^^^end session storage^^^^^^\\
-
 $(function() {
   //------start countdown clock----------\\
   var clock = $('.clock').FlipClock({
